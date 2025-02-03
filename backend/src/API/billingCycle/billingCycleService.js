721ac29a2155a8bc0568e425e3c652ad6f6d51ed
Lucas Brito <35507273+britolmbs@users.noreply.github.com>
2025-02-03 14:59:52 -0300

    Obtendo a Quantidade de Registros (Count)

--- backend/src/API/billingCycle/billingCycleService.js
+++ backend/src/API/billingCycle/billingCycleService.js
@@ -1,3 +1,4 @@
+const billingCycle = require('./billingCycle')
 const BillingCycle = require('./billingCycle')
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({new: true, runValidators: true})
@@ -8,6 +9,16 @@ BillingCycle.route('get', (req, res, next) => {
             res.json(docs)
         } else{
             res.status(500).json({errors: [error]})
+        }
+    })
+})
+
+BillingCycle.route('count', (req, res, next) => {
+    billingCycle.count((error, value) => {
+        if(error) {
+            res.status(500).json({errors: [error]})
+        }else {
+            res.json({value})
         }
     })
 })
