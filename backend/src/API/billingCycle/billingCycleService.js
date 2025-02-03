f491bcdf2c51cbba2f7228a5d48eab40a993475e
Lucas Brito <35507273+britolmbs@users.noreply.github.com>
2025-02-03 15:18:53 -0300

    Correção de erro no Cursor ou Pipeline

--- backend/src/API/billingCycle/billingCycleService.js
+++ backend/src/API/billingCycle/billingCycleService.js
@@ -23,4 +23,20 @@ BillingCycle.route('count', (req, res, next) => {
     })
 })
 
+BillingCycle.route('summary', (req, res, next) => {
+    billingCycle.aggregate([{
+        $projetc: {credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"}}
+    }, {
+        $group: {_id: null, credit: {$sum: "$credit", debt: {$sum: "$debt"}}}
+    }, {
+        $projetc: {_id: 0, credit: 1, debt: 1}
+    }], (error, result) => {
+        if(error) {
+            res.status(500).json({errors: [error]})
+        } else {
+            res.json(result[0] || {credit: 0, debt: 0})
+        }
+    })
+})
+
 module.exports = BillingCycle
