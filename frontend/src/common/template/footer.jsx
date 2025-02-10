fa502021cbc86235f0168a2dd34e78505ae38d53
Lucas Brito <35507273+britolmbs@users.noreply.github.com>
2025-02-10 14:54:48 -0300

    Componente Footer

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright Lucas Brito &copy; 2025
+            <a href='https://www.linkedin.com.br/in/britolmbs' target='_blank'> Lucas Brito </a>
+        </strong>
+    </footer>
+)
