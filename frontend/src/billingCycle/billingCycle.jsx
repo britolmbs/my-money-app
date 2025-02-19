2cc22b986ad26f13a6f8aebd5d8b0b57167aa7f5
Lucas Brito <britolmbs@hotmail.com>
2025-02-19 20:47:59 -0300

    Componente Tab Content

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,10 +1,11 @@
-import React, {Component} from "react";
+import React, {Component} from "react"
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
-import Tabs from "../common/tab/tabs";
-import TabsHeader from "../common/tab/tabsHeader";
-import TabsContent from "../common/tab/tabsContent";
-import tabHeader from "../common/tab/tabHeader";
+import Tabs from "../common/tab/tabs"
+import TabsHeader from "../common/tab/tabsHeader"
+import TabsContent from "../common/tab/tabsContent"
+import TabHeader from "../common/tab/tabHeader"
+import TabContent from "../common/tab/tabContent"
 
 class BillingCycle extends Component {
     render (){
@@ -14,13 +15,16 @@ class BillingCycle extends Component {
                 <Content>
                     <Tabs>
                         <TabsHeader>
-                        <TabHeader label='Listar' icon='bars' target='tabList' />
+                        <tabHeader label='Listar' icon='bars' target='tabList' />
                         <TabHeader label='Incluir' icon='plus' target='tabCreate' />
                         <TabHeader label='Alterar' icon='pencil' target='tabUpdate' />
                         <tabHeader label='Excluir' icon='trash-o' target='tabDelede' />
                         </TabsHeader>
                         <TabsContent>
-                            
+                            <TabContent id='tabList'><h1>Lista</h1></TabContent>
+                            <TabContent id='tabCreate'><h1>Incluir</h1></TabContent>
+                            <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
+                            <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
                         </TabsContent>
                     </Tabs>
                 </Content>
