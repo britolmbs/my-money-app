c9bc0ac2456f3ec6a79a58099cdc5f4ad66b08df
Lucas Brito <35507273+britolmbs@users.noreply.github.com>
2025-02-24 11:43:08 -0300

    Criando BillingCycleForm com Redux-Form

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -10,6 +10,7 @@ import TabsContent from "../common/tab/tabsContent"
 import TabHeader from "../common/tab/tabHeader"
 import TabContent from "../common/tab/tabContent"
 import List from "./billingCycleList"
+import Form from "./billingCycleForm"
 
 class BillingCycle extends Component {
     componentWillMount() {
@@ -30,7 +31,7 @@ class BillingCycle extends Component {
                         </TabsHeader>
                         <TabsContent>
                             <TabContent id='tabList'><List /></TabContent>
-                            <TabContent id='tabCreate'><h1>Incluir</h1></TabContent>
+                            <TabContent id='tabCreate'><Form /></TabContent>
                             <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
                             <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
                         </TabsContent>
