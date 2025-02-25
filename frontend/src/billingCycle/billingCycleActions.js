67127076445abdbc1b11bf6f0ec3eb646ffeac64
Lucas Brito <35507273+britolmbs@users.noreply.github.com>
2025-02-25 11:11:06 -0300

    Exibindo as Mensagens  de Sucesso e Erro

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { toastr } from 'react-redux-toastr'
 const BASE_URL = 'https://localhost:3003/api'
 
 export function getList() {
@@ -11,6 +12,12 @@ export function getList() {
 
 export function create(values) {
     axios.post(`${BASE_URL}/billingCycle`, values)
+    .then(resp => {
+        toastr.success('Sucesso', 'Operação Realizada com sucesso.')
+    })
+    .catch(e => {
+        e.response.data.errors.foreach(error => toastr.error('Erro', error))
+    })
     return{
         type: 'TEMP'
     }
