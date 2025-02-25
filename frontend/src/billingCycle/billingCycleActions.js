46b344fa1d7455f6692bed1dee58cc2c854b5e9a
Lucas Brito <35507273+britolmbs@users.noreply.github.com>
2025-02-25 15:03:45 -0300

    Melhorias na Inclusão do Ciclo de pagamento

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
 import { toastr } from 'react-redux-toastr'
+import { reset as resetForm } from 'redux-form'
+import { showTabs, selectTab } from '../common/tab/tabActions'
+
 const BASE_URL = 'https://localhost:3003/api'
 
 export function getList() {
@@ -11,14 +14,21 @@ export function getList() {
 }
 
 export function create(values) {
-    axios.post(`${BASE_URL}/billingCycle`, values)
-    .then(resp => {
-        toastr.success('Sucesso', 'Operação Realizada com sucesso.')
-    })
-    .catch(e => {
-        e.response.data.errors.foreach(error => toastr.error('Erro', error))
-    })
-    return{
-        type: 'TEMP'
+    return dispatch => {
+        axios.post(`${BASE_URL}/billingCycle`, values)
+        .then(resp => {
+            toastr.success('Sucesso', 'Operação Realizada com sucesso.')
+            dispatch([
+                resetForm('billingCycleForm'),
+                getList(),
+                selectTab('tabList'),
+                showTabs('tabList', 'tabCreate')
+            ])
+        })
+        .catch(e => {
+            e.response.data.errors.foreach(error => toastr.error('Erro', error))
+        })
+
     }
+   
 }
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -5,10 +5,12 @@ import { applyMiddleware ,createStore } from "redux";
 import { Provider } from 'react-redux'
 import reducers from "./main/reducers";
 import promisse from 'redux-promise'
+import multi from 'redux-multi'
+import thunk from 'redux-thunk'
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
 && window.__REDUX_DEVTOOLS_EXTENSION__()
-const store = applyMiddleware(promisse)(createStore)(reducers, devTools)
+const store = applyMiddleware(multi, thunk, promisse)(createStore)(reducers, devTools)
 ReactDOM.render(
     <Provider store={store}>
          <App />
