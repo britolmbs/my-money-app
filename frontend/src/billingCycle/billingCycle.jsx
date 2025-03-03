800784f2109b17df0fd4bdfd62949566b73af284
Lucas Brito <britolmbs@hotmail.com>
2025-03-03 18:13:06 -0300

    Evoluindo o Componente CreditList

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,7 +1,6 @@
 import React, {Component} from "react"
 import { bindActionCreators } from "redux"
 import { connect } from "react-redux"
-import { selectTab, showTabs } from "../common/tab/tabActions"
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 import Tabs from "../common/tab/tabs"
@@ -11,12 +10,11 @@ import TabHeader from "../common/tab/tabHeader"
 import TabContent from "../common/tab/tabContent"
 import List from "./billingCycleList"
 import Form from "./billingCycleForm"
-import { create, update, remove } from "./billingCycleActions"
+import { create, update, remove, init } from "./billingCycleActions"
 
 class BillingCycle extends Component {
     componentWillMount() {
-        this.props.selectTab('tabList')
-        this.props.showTabs('tabList', 'tabCreate')
+      this.props.init
     }
     render (){
         return (
@@ -48,5 +46,5 @@ class BillingCycle extends Component {
         )
     }
 }
-const mapDispatchToProps = dispatch => bindActionCreators({selectTab, showTabs, create, update, remove}, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({create, update, remove, init}, dispatch)
 export default connect(null, mapDispatchToProps)(BillingCycle)
