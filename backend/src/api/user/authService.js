9b4cc5923c2f06243518ba3475d728147855bdc8
Lucas Brito <britolmbs@hotmail.com>
2025-03-09 06:53:36 -0300

    criação do metodo signup

--- backend/src/api/user/authService.js
+++ backend/src/api/user/authService.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const User = require('./user')
 const env = require('../../.env')
+const user = require('./user')
 
 const emailRegex = /\S+@\S+\.\S+/
 const passwordRegex = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})/
@@ -38,4 +39,46 @@ const validateToken = (req, res, next) => {
         return res.status(200).send({ valid: !err})
     })
 }
+
+const signup = (req, res, next) => {
+    const name = req.body.name || ''
+    const email = req.body.email || ''
+    const password = req.body.password || ''
+    const confirmPassword = req.body.confirm_password || ''
+    
+    if (!email.match(emailRegex)) {
+        return res.status(400).send({ errors : ['O email informado está invalido'] })
+    }
+
+    if (!password.match(passwordRegex)){
+        return res.status(400).send({
+            errors: ["Senha precisar ter: uma letra maiúscula, uma letra minúscula, um número, uma caracter especial(@#$%) e tamanho entre 6-20"]
+        })     
+    }
+        const salt = bcrypt.genSaltSync()
+        const passwordHash = bcrypt.hashSync(password, salt)
+        if (!bcrypt.compareSync(confirmPassword, passwordHash)) {
+            return res.status(400).send({ errors: ['Senhas não conferem'] })
+        } 
+
+    User.findOne({email}, (err, user) => {
+        if(err) {
+            return sendErrorsFromDB(res, err)
+        } else if (user) {
+            return res.status(400).send({ errors: ['Usuário já cadastrado'] })
+        } else {
+            const newUser = new User({ name, email, password: passwordHash })
+            newUser.save(err =>{
+                if (err) {
+                    return sendErrorsFromDB(res, err) 
+                } else {
+                    login(req, res, next)
+                }
+            })
+        }
+
+    })
+}
+
+module.exports = { login, signup, validateToken }
 
