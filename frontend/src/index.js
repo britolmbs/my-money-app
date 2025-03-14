6e8e6ef954753d7bb3898ecbfa29a8c935218fd0
Lucas Brito <35507273+britolmbs@users.noreply.github.com>
2025-03-14 10:50:54 -0300

    Melhorias no codigo

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from 'react-dom'
-import Routes from "./main/routes";
 import { applyMiddleware ,createStore } from "redux";
 import { Provider } from 'react-redux'
 import reducers from "./main/reducers"
@@ -8,13 +7,14 @@ import promisse from 'redux-promise'
 import multi from 'redux-multi'
 import thunk from 'redux-thunk'
 import registerServiceWorker from './registerServiceWorker'
+import AuthOrApp from './main/authOrApp'
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
 && window.__REDUX_DEVTOOLS_EXTENSION__()
 const store = applyMiddleware(multi, thunk, promisse)(createStore)(reducers, devTools)
 ReactDOM.render(
     <Provider store={store}>
-        <Routes />
+       <AuthOrApp />
     </Provider>
 , document.getElementById('app'))
 registerServiceWorker()
