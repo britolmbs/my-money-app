fb5618c852d516f30d832b29009836afb26870db
Lucas Brito <britolmbs@hotmail.com>
2025-05-25 13:07:23 -0300

    correção de codigo

--- frontend/src/auth/auth.jsx
+++ frontend/src/auth/auth.jsx
@@ -9,53 +9,24 @@ import Grid from '../common/layout/grid'
 import If from '../common/operator/if'
 import Messages from '../common/msg/messages'
 import Input from '../common/form/inputAuth'
-
 class Auth extends Component {
-    state = { loginMode: true };
-  
-changeMode = () =>  {
-    this.setState(prevState => ({ loginMode: !this.state.loginMode }));
-};
-onSubmit = (values) =>  {
-    const { login, signup } = this.props;
-    this.state.loginMode ? login(values) : signup(values);
-};
-render() {
-    const { loginMode } = this.state;
-    const { handleSubmit } = this.props;
-
-    return (
-        <div className="login-box">
-            <div className="login-logo"><b>My</b>Money</div>
-            <div className="login-box-body">
-                <p className="login-box-msg">Bem Vindo!</p>
-                <form onSubmit={handleSubmit(this.onSubmit)}>
-                    <Field component={Input} type='input' name='name'
-                    placeholder='Nome' icon='user' hide={loginMode} />
-                    <Field component={Input} type='email' name='email'
-                    placeholder='Email' icon='envelope' />
-                    <Field component={Input} type='password' name='password'
-                    placeholder='Senha' icon='lock' />
-                    <Field component={Input} type='password' name='confirm_password'
-                    placeholder='Confirma Senha' icon='lock' hide={loginMode} />
-                    <Row>
-                        <Grid cols='4'>
-                            <button type='submit' className='btn btn-primary btn-block btn-flat'>
-                                {loginMode ? 'Entrar' : 'Registrar'}
-                            </button>
-                        </Grid>
-                    </Row>
-                </form>
-                <br />
-                <a onClick={this.changeMode}>
-                    {loginMode ? 'Novo usuário? Registrar aqui!' : 'Já é Cadastrado? Entrar aqui!'}
-                </a>
-            </div>
-            <Messages />
-        </div>
-
-    )
-    }
+  constructor(props) {
+  super(props)
+  this.state = {loginMode: true}
+  }
+  changeMode() {
+  this.setState({ loginMode: !this.state.loginMode })
+  }
+  onSubmit(values) {
+  const { login, signup } = this.props
+  this.state.loginMode ? login(values) : signup(values)
+  }
+  render() {
+  const { loginMode } = this.state
+  const { handleSubmit } = this.props
+  }
 }
-
-export default connect(null, mapDispatchToProps)(reduxForm({ form: 'authForm'})(Auth));
+Auth = reduxForm({form: 'authForm'})(Auth)
+const mapDispatchToProps = dispatch => bindActionCreators({ login, signup },
+dispatch)
+export default connect(null, mapDispatchToProps)(Auth)
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,82 +1,47 @@
-const path = require('path');
-const webpack = require('webpack');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-    // Ponto de entrada do aplicativo
     entry: './src/index.jsx',
-
-    // Configurações de saída do bundle
     output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'app.js',
-        clean: true, // Limpa a pasta de saída antes de gerar novos arquivos
+        path: __dirname + '/public',
+        filename: './app.js'
     },
-
-    // Modo de desenvolvimento
-    mode: 'development', // Você pode mudar para 'production' quando necessário
-
-    // Configurações do Dev Server
     devServer: {
         port: 8080,
-        static: {
-            directory: path.resolve(__dirname, 'public'),
-        },
-        hot: true,
-        historyApiFallback: true, // Para suportar o React Router
+        contentBase: './public',
     },
-
-    // Resolução de módulos
     resolve: {
-        extensions: ['.js', '.jsx'],
+        extensions: ['', '.js', '.jsx'],
         alias: {
-            '@': path.resolve(__dirname, 'src'), // Adicione esta linha para o alias
-            modules: path.resolve(__dirname, 'node_modules'),
-            jquery: path.resolve(__dirname, 'node_modules/admin-lte/plugins/jQuery/jquery-4.0.0-beta3.min.js'),
-            bootstrap: path.resolve(__dirname, 'node_modules/admin-lte/bootstrap/js/bootstrap.js'),
-        },
+            modules: __dirname + '/node_modules',
+            jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
+            bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
+        }
     },
-
-    // Plugins
-    plugins: [
+    plugins: [ 
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery',
-            'window.jQuery': 'jquery',
+            'window.jQuery': 'jquery'
         }),
-        new MiniCssExtractPlugin({
-            filename: 'app.css',
-        }),
+        new ExtractTextPlugin('app.css')
     ],
-
-    // Regras para os loaders
     module: {
-        rules: [
-            {
-                // Loader para Babel
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react'],
-                        plugins: ['@babel/plugin-proposal-object-rest-spread'],
-                    },
-                },
-            },
-            {
-                // Loader para CSS
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                ],
-            },
-            {
-                // Loader para fontes e imagens
-                test: /\.(woff|woff2|ttf|eot|svg|png|jpg)$/,
-                type: 'asset/resource',
-            },
-        ],
-    },
-};
+        loaders: [{
+            test: /.js[x]?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react'],
+                plugins: ['transform-object-rest-spread']
+            }
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+        }, {
+            test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
+            loader: 'file'
+        }]
+    }
+}
